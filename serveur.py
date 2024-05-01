98d34fc7c1eaadef1f6d0fef60ec5d0b873f4478
lamyae-fakir <59096036+lamyae-fakir@users.noreply.github.com>
2024-05-01 22:48:41 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -1,14 +1,38 @@
-import socket 
-s=socket.socket()
-print('socket crée ')
-port = 56789
-s.bind(('', port))
-print(f'socket binded au port {port}')
-s.listen(5)
-print("socket is listening")
-while True:
-    c,addr =s.accept()
-    print = ('connexion a partir de ', addr)
-    message = ('merci pour la connexion')
-    c.send(message.encode())
-    c.close()
+import socket,select
+
+HOST = "127.0.0.1" # or 'localhost' or '' - Standard loopback interface address
+PORT = 2000 # Port to listen on (non-privileged ports are > 1023)
+MAXBYTES = 4096
+
+
+
+# create socket
+serversocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    # AF_INET: IPv4
+    # SOCK_STREAM: TCP
+
+
+serversocket.bind((HOST, PORT)) # bind this socket to specific port on host
+serversocket.listen() # make the socket a listening one
+#(clientsocket,(addr,port,),) = serversocket.accept() # blocking; returns if a client connects.
+socketlist = [serversocket]
+
+
+
+while len(socketlist) > 0:
+    (readable, _, _) = select.select(socketlist, [], [])
+    for s in readable:
+        print("desc", s.fileno())
+        if s == serversocket: # serversocket receives a connection
+            (clientsocket, (addr, port)) = s.accept()
+            print("connection from:", port)
+            socketlist.append(clientsocket)
+        else: # data is sent from given client
+            data = s.recv(MAXBYTES)
+            if len(data) > 0:
+                s.sendall(data)
+            else: # client has disconnected
+                s.close()
+                socketlist.remove(s)
+                
+serversocket.close()
