976750aec2102308437f03209dd4865294271a8b
lamyae-fakir <59096036+lamyae-fakir@users.noreply.github.com>
2024-05-01 22:46:45 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,23 @@
-import socket
-s=socket.socket()
-port = 56789
-s.connect(("127.0.0.1", port))
-print(s.recv(1024))
+import os, socket, sys
+MAXBYTES = 4096
+if len(sys.argv) != 3:
+    print("Usage:", sys.argv[0], "hote port")
+    sys.exit(1)
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+sockaddr = (HOST, PORT)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPv4, TCP
+s.connect(sockaddr)
+print("connected to:", sockaddr)
+while True: # Client synchrone !! On alterne écriture vers serveur
+# et lecture depuis serveur. Le serveur doit donc lui aussi alterner
+    line = os.read(0, MAXBYTES)
+    if len(line) == 0:
+        s.shutdown(socket.SHUT_WR)
+        break
+    s.send(line)
+    data = s.recv(MAXBYTES) # attention, si le serveur n'envoie rien on est bloqué.
+    if len(data) == 0:
+        break
+    os.write(1, data)
 s.close()
